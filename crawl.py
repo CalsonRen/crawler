fd56aa6e4e509c8f12456917849cebe0ff0bbc8b
renjie <calsonren@gmail.com>
2017-01-03 09:30:43 +0800

    A simple web crawler

---
+++ crawl.py
@@ -0,0 +1,115 @@
+#!usr/bin/env python3.5
+"""
+Author: renjie
+Project: web crawler
+"""
+
+# TODO:
+# -Add arguments to specify TLS setting
+
+import argparse
+import asyncio
+import sys
+import logging
+import reporting
+import crawling
+
+ARGS = argparse.ArgumentParser(description="Web crawler")
+ARGS.add_argument(
+	'--iocp', action='store_true', dest='iocp',
+        default=False, help='Use IOCp event loop(Windows only)'
+)
+ARGS.add_argument(
+	'--select', action='store_true', dest='select',
+        default=False, help='Use select event loop instead of default'
+)
+ARGS.add_argument(
+	'roots', nargs='*',
+        default=[], help='Root URL (may be repeated)'
+)
+
+ARGS.add_argument(
+	'--max_redirect', action='store', type=int, metavar='N',
+        default=10, help='Limit redirection chains(for 301, 302 etc.)'
+)
+ARGS.add_argument(
+        '--max_tries', action='store', type=int, metavar='N',
+        default=4, help='Limit retries on networks errors'
+)
+ARGS.add_argument(
+        '--max_tasks', action='store', type=int, metavar='N',
+        default=100, help='Limit concurrent connection'
+)
+ARGS.add_argument(
+        '--exclude', action='store', metavar='REGEX',
+        help='Exclude matching URLs'
+)
+ARGS.add_argument(
+        '--strict', action='store_true',
+        default=True, help='Strict host matching(default)'
+        )
+ARGS.add_argument(
+        '--leninet', action='store_false', dest='strict',
+        default=False, help='Lenient host matching'
+        )
+ARGS.add_argument(
+        '-v', '--verbose', action='count', dest='level',
+        default=2, help='Verbose logging(repeat for more verbose)'
+        )
+ARGS.add_argument(
+        '-q', '--quiet', action='store_const', const=0, dest='level', default=2, help='Only log errors'
+        )
+
+
+def fix_url(url):
+    """Prefix a schema-less URL with https://."""
+    if '://' not in url:
+        url = 'http://' + url
+    return url
+
+
+def main():
+    """Main program
+    Parse arguments, set up event loop, run crawler, print report.
+    """
+
+    args = ARGS.parse_args()
+    if not args.roots:
+        print('Use --help for command line help')
+        return
+
+    levels = [logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG]
+    logging.basicConfig(level=levels[min(args.level, len(levels)-1)])
+    if args.iocp:
+        from asyncio.windows_events import ProactorEventLoop
+        loop = ProactorEventLoop()
+        asyncio.set_event_loop(loop)
+    elif args.select:
+        loop = asyncio.SelectorEventLoop()
+        asyncio.set_event_loop(loop)
+    else: 
+        loop = asyncio.get_event_loop()
+
+    roots = {fix_url(root) for root in args.roots}
+
+    crawler = crawling.Crawler(roots,
+                               exclude=args.exclude,
+                               strict=args.strict,
+                               max_redirect=args.max_redirect,
+                               max_tries=args.max_tries,
+                               max_tasks=args.max_tasks,
+                               )
+    try:
+        loop.run_until_complete(crawler.crawl())
+    except KeyboardInterrupt:
+        sys.stderr.flush()
+        print('\nInterrupted\n')
+    finally:
+        reporting.report(crawler)
+        crawler.close()
+        loop.stop()
+        loop.run_forever()
+        loop.close()
+
+if __name__ == '__main__':
+    main()
--- readme.txt
+++ readme.txt
@@ -1,3 +1,17 @@
-titile: crawler created by python
-author: renjie
+Project: crawler created by python
+Author: renjie
 date: 2016/12/31
+
+Requirements:
+  *python 3.4+
+  *aiohttp
+
+This is a web crawler, you give it a URL and it will craw that website by following href links in the HTML pages.
+
+It doesn't do anything with the crawled pages, and the algorithm for finding links is intentionally naive -- these parts are easily modifie, and not of particular interes.
+
+The point of this projet is to show off how to write a reasonably complex HTTP client application using the asynixo module. 
+
+usage:
+
+  python3 crawl.py -q xkcd.com
